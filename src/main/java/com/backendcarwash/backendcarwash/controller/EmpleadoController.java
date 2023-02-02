a5afc3ea38dc5effaf5f371b03b97b42a759ccef
Mrzz98 <107141281+Mrzz98@users.noreply.github.com>
2023-02-01 22:15:50 -0300

    Update EmpleadoController.java

--- src/main/java/com/backendcarwash/backendcarwash/controller/EmpleadoController.java
+++ src/main/java/com/backendcarwash/backendcarwash/controller/EmpleadoController.java
@@ -11,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("/empleados")
 public class EmpleadoController {
 
